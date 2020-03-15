f969bd27844d50c8c9a9692e9b1a66b36b2b0486
atsushi-kitazawa <atsushi-kitazawa>
2020-03-16 00:23:23 +0900

    aws health dashborad page scraping (still in progress)

--- cmd/sampleapp/main.go
+++ cmd/sampleapp/main.go
@@ -3,43 +3,100 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	// "github.com/kr/pretty"
 )
 
+var regionCode = []string{"us-east-1",
+	"us-east-2",
+	"us-west-2",
+	"us-west-1",
+	"ca-central-1",
+	"sa-east-1",
+	"eu-west-1",
+	"eu-central-1",
+	"eu-west-2",
+	"eu-west-3",
+	"eu-north-1",
+	"me-south-1",
+	"ap-southeast-1",
+	"ap-northeast-1",
+	"ap-southeast-2",
+	"ap-northeast-2",
+	"ap-south-1",
+	"ap-east-1"}
+
+var regionRssMap = map[string][]string{}
+
 func main() {
-	// helloworld()
+	for _, value := range regionCode {
+		regionRssMap[value] = []string{}
+	}
+	// DEBUG
+	// pretty.Printf("--- m:\n%# v\n\n", regionRssMap)
 
-	// var a, b int = 10, 20
-	// var c = add(a, b)
-	// fmt.Printf("%v\n", c)
-
-	readFile("input")
+	GetPage("https://status.aws.amazon.com/")
+	// GetPage("http://localhost/test.html")
 }
 
-func helloworld() {
-	msg1 := "hello"
-	msg2 := "world."
-	fmt.Printf("%v\n", msg1+" "+msg2)
-}
-
-func add(a int, b int) int {
-	return a + b
-}
-
-func readFile(file string) {
-	f, err := os.Open(file)
+// GetPage return void
+func GetPage(url string) {
+	file, err := os.Create("output_rss.txt")
 	if err != nil {
-		fmt.Printf("error")
+		fmt.Println("error")
 	}
-	defer f.Close()
-	buf := make([]byte, 1024)
-	for {
-		n, err := f.Read(buf)
-		if n == 0 {
-			break
+	defer file.Close()
+	doc, _ := goquery.NewDocument(url)
+	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
+		url, _ := s.Attr("href")
+		switch {
+		case strings.HasSuffix(url, "us-east-1.rss"):
+			regionRssMap["us-east-1"] = append(regionRssMap["us-east-1"], url)
+		case strings.HasSuffix(url, "us-east-2.rss"):
+			regionRssMap["us-east-2"] = append(regionRssMap["us-east-2"], url)
+		case strings.HasSuffix(url, "us-west-2.rss"):
+			regionRssMap["us-west-2"] = append(regionRssMap["us-west-1"], url)
+		case strings.HasSuffix(url, "us-west-1.rss"):
+			regionRssMap["us-west-1"] = append(regionRssMap["us-west-1"], url)
+		case strings.HasSuffix(url, "ca-central-1.rss"):
+			regionRssMap["ca-central-1"] = append(regionRssMap["ca-central-1"], url)
+		case strings.HasSuffix(url, "sa-east-1.rss"):
+			regionRssMap["sa-east-1"] = append(regionRssMap["sa-east-1"], url)
+		case strings.HasSuffix(url, "eu-west-1.rss"):
+			regionRssMap["eu-west-1"] = append(regionRssMap["eu-west-1"], url)
+		case strings.HasSuffix(url, "eu-central-1.rss"):
+			regionRssMap["eu-central-1"] = append(regionRssMap["eu-central-1"], url)
+		case strings.HasSuffix(url, "eu-west-2.rss"):
+			regionRssMap["eu-west-2"] = append(regionRssMap["eu-west-2"], url)
+		case strings.HasSuffix(url, "eu-west-3.rss"):
+			regionRssMap["eu-west-3"] = append(regionRssMap["eu-west-3"], url)
+		case strings.HasSuffix(url, "eu-north-1.rss"):
+			regionRssMap["eu-north-1"] = append(regionRssMap["eu-north-1"], url)
+		case strings.HasSuffix(url, "me-south-1.rss"):
+			regionRssMap["me-south-1"] = append(regionRssMap["me-south-1"], url)
+		case strings.HasSuffix(url, "ap-southeast-1.rss"):
+			regionRssMap["ap-southeast-1"] = append(regionRssMap["ap-southeast-1"], url)
+		case strings.HasSuffix(url, "ap-northeast-1.rss"):
+			regionRssMap["ap-northeast-1"] = append(regionRssMap["ap-northeast-1"], url)
+		case strings.HasSuffix(url, "ap-southeast-2.rss"):
+			regionRssMap["ap-southeast-2"] = append(regionRssMap["ap-southeast-2"], url)
+		case strings.HasSuffix(url, "ap-northeast-2.rss"):
+			regionRssMap["ap-northeast-2"] = append(regionRssMap["ap-northeast-2"], url)
+		case strings.HasSuffix(url, "ap-south-1.rss"):
+			regionRssMap["ap-south-1"] = append(regionRssMap["ap-south-1"], url)
+		case strings.HasSuffix(url, "ap-east-1.rss"):
+			regionRssMap["ap-east-1"] = append(regionRssMap["ap-east-1"], url)
 		}
-		if err != nil {
-			break
+	})
+
+	for key, value := range regionRssMap {
+		file.WriteString(key + "\r\n")
+		for _, value1 := range value {
+			file.WriteString("     " + value1 + "\r\n")
 		}
-		fmt.Println(string(buf[:n]))
 	}
+
+	// pretty.Printf("--- m:\n%# v\n\n", regionRssMap)
 }
--- sampleapp.go
+++ sampleapp.go
@@ -3,7 +3,46 @@ package sampleapp
 import (
 	"fmt"
 	"io/ioutil"
+
+	"github.com/PuerkitoBio/goquery"
 )
+
+// TODO
+// リージョンの日本語表記と英語表記のマッピング
+// サービスの日本語表記と英語表記のマッピング
+// スクレイピング
+// - CSV形式にする
+// dat ファイルを作成する
+
+var resionToCode = map[string]string{"Northern Virginia": "us-east-1",
+	"Ohio":                "us-east-2",
+	"Oregon":              "us-west-2",
+	"Northern California": "us-west-1",
+	"Montreal":            "ca-central-1",
+	"São Paulo":           "sa-east-1",
+	"Ireland":             "eu-west-1",
+	"Frankfurt":           "eu-central-1",
+	"London":              "eu-west-2",
+	"Paris":               "eu-west-3",
+	"Stockholm":           "eu-north-1",
+	"Bahrain":             "me-south-1",
+	"Singapore":           "ap-southeast-1",
+	"Tokyo":               "ap-northeast-1",
+	"Sydney":              "ap-southeast-2",
+	"Seoul":               "ap-northeast-2",
+	"Mumbai":              "ap-south-1",
+	"Hong Kong":           "ap-east-1"}
+
+var serviceToCode = map[string]string{"aaa": "AAA"}
+
+// GetPage return void
+func GetPage(url string) {
+	doc, _ := goquery.NewDocument(url)
+	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
+		url, _ := s.Attr("href")
+		fmt.Println(url)
+	})
+}
 
 func helloworld() {
 	msg1 := "hello"
